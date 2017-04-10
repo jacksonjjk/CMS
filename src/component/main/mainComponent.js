88e635f0932dbca1ba4737a20ca96c1fa3041bfb
Hezon <jj72835@lookout.com>
2017-04-10 20:55:00 +0800

    add by jy

--- src/component/main/mainComponent.js
+++ src/component/main/mainComponent.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var stype = require('./mainComponent.css');
+var $ = require('jquery');
 
 var ReactRouter = require("react-router");
 var {Router, Route, hashHistory, Link, IndexRoute, browserHistory} = ReactRouter;
@@ -7,18 +8,36 @@ var {Router, Route, hashHistory, Link, IndexRoute, browserHistory} = ReactRouter
 var MainComponent = React.createClass({
 	componentWillMount: function() {
 		//判断是当前用户是否有权限，如果没有，则跳车到 login
-		console.log(2)
+		
 	},
 	componentDidMount() {
-		console.log(this.props.router)
-		console.log(this.props.route)
-		console.log(this.props)
+		
 	},
-	
+	hiddenSide(){
+		if($('.menu').css('display') === 'none'){
+			$('.menu').css('display','block');
+			$('#content').css('width','85%');
+			$('.hiddenSide span').html('隐藏导航栏');
+		}else{
+			$('.menu').css('display','none');
+			$('#content').css('width','100%');
+			$('.hiddenSide span').html('显示导航栏');
+		}
+	 	
+	 	
+	},
 	render: function(){
 		return (
 			<div className="container">
-				<div className="header"></div>
+				<div className="header clear">
+					<div className="logo"><img src="./src/component/main/img/logo.png" alt=""/></div>
+					<div className="hiddenSide" onClick={this.hiddenSide}><span>隐藏导航栏</span></div>
+					<div className="msg">
+						<span className="m_user">爱国少年</span>
+						<span className="m_test">测试</span>
+						<span className="m_logout">注册</span>
+					</div>
+				</div>
 				<div className="body clear">
 					<ul className="menu">
 						<li className="menu-item"><Link to="/summarizing">销售汇总</Link></li>
